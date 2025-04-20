09d5710f3f91471833d3b50b924731bab69c8350
unknown <abishekpriyan11@gmail.com>
2025-04-20 01:22:51 -0400

    modified .env

--- client/src/apollo/client.js
+++ client/src/apollo/client.js
@@ -1,7 +1,18 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+// src/graphql/client.js
+
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+// Pick up from Vite’s env, or fall back to localhost for `npm run dev`
+const GRAPHQL_URL =
+  import.meta.env.VITE_GRAPHQL_URL || "http://localhost:4000/graphql";
+
+const link = createHttpLink({
+  uri: GRAPHQL_URL,
+  credentials: "include",   // change to "same-origin" or remove if you don't need cookies
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",  
+  link,
   cache: new InMemoryCache(),
 });
 
